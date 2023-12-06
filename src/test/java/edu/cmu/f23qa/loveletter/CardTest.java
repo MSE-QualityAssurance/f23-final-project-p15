3e9192ea866f74919efb531eaf8c2bf534f95a40
tianqiZ0507 <148793678+tianqiZ0507@users.noreply.github.com>
2023-12-06 05:02:40 +0000

    Updated three Mockito version of Baron test case, deleted the protected senario

--- src/test/java/edu/cmu/f23qa/loveletter/CardTest.java
+++ src/test/java/edu/cmu/f23qa/loveletter/CardTest.java
@@ -23,17 +23,17 @@ public class CardTest {
      * My card is a Priest, and  [normal player] is a Guard. 
      * Since my card has a higher value, the [normal player] is knocked out of the round.
      */
-    // @Test
-    // void testBaronUserWins(){
-    //     Player user = new Player("User");
-    //     user.getHand().add(Card.PRIEST);
-    //     Player opponent = new Player("Opponent");
-    //     opponent.getHand().add(Card.GUARD);
-    //     GameActions gameActions = new GameActions() {}; 
-    //     gameActions.useBaron(user, opponent);
-    //     Assertions.assertTrue(opponent.getHand().hasCards() == false, 
-    //         "Opponent should be eliminated");
-    // }
+    @Test
+    void testBaronUserWins(){
+        Player user = new Player("User");
+        user.getHand().add(Card.PRIEST);
+        Player opponent = new Player("Opponent");
+        opponent.getHand().add(Card.GUARD);
+        GameActions gameActions = new GameActions() {}; 
+        gameActions.useBaron(user, opponent);
+        Assertions.assertTrue(opponent.getHand().hasCards() == false, 
+            "Opponent should be eliminated");
+    }
 
     /*
      * After drawing, I discard the Baron and choose [normal player].
@@ -68,29 +68,29 @@ public class CardTest {
             && opponent.getHand().hasCards(), "Nothing happens");    
     }
 
-    /*
-     * I cannot use the Baron on a [abnormal player] 
-     * who is currently protected by a Handmaid.
-     */
-    @Test
-    void testBaronProtected(){
-        Player user = new Player("User");
-        user.getHand().add(Card.KING);
-        Player opponent = new Player("Opponent");
-        opponent.getHand().add(Card.KING);
-        opponent.switchProtection();
-        GameActions gameActions = new GameActions() {}; 
-        gameActions.useBaron(user, opponent);
-        Assertions.assertTrue(user.getHand().hasCards() == true 
-            && opponent.getHand().hasCards() == true, "Nothing happens");    
-    }
+    // /*
+    //  * I cannot use the Baron on a [abnormal player] 
+    //  * who is currently protected by a Handmaid.
+    //  */
+    // @Test
+    // void testBaronProtected(){
+    //     Player user = new Player("User");
+    //     user.getHand().add(Card.GUARD);
+    //     Player opponent = new Player("Opponent");
+    //     opponent.getHand().add(Card.GUARD);
+    //     opponent.switchProtection();
+    //     GameActions gameActions = new GameActions() {}; 
+    //     gameActions.useBaron(user, opponent);
+    //     Assertions.assertTrue(user.getHand().hasCards()
+    //         && opponent.getHand().hasCards(), "Nothing happens");    
+    // }
         /*
      * After drawing, I discard the Baron and choose  [normal player]. 
      * My card is a Priest, and  [normal player] is a Guard. 
      * Since my card has a higher value, the [normal player] is knocked out of the round.
      */
     @Test
-    void testBaronUserWins(){
+    void testBaronUserWinsMock(){
         // Mocking the players and their hands
         Player user = Mockito.mock(Player.class);
         Player opponent = Mockito.mock(Player.class);
@@ -111,5 +111,80 @@ public class CardTest {
 
         // Assertion: Opponent should be eliminated
         Assertions.assertTrue(opponent.getHand().hasCards() == false, "Opponent should be eliminated");
+    }
+
+    @Test
+    void testBaronOpponentWinsMock(){
+        // Mocking the players and their hands
+        Player user = Mockito.mock(Player.class);
+        Player opponent = Mockito.mock(Player.class);
+
+        Hand userHand = Mockito.mock(Hand.class);
+        Hand opponentHand = Mockito.mock(Hand.class);
+
+        // Setting up the hands
+        when(user.getHand()).thenReturn(userHand);
+        when(opponent.getHand()).thenReturn(opponentHand);
+
+        when(userHand.peek(0)).thenReturn(Card.KING);
+        when(opponentHand.peek(0)).thenReturn(Card.GUARD);
+        
+        // Action: User uses Baron on opponent
+        GameActions gameActions = new GameActions() {}; 
+        gameActions.useBaron(user, opponent);
+
+        // Assertion: Opponent should be eliminated
+        Assertions.assertTrue(user.getHand().hasCards() == false, "User should be eliminated");
+    }
+
+    @Test
+    void testBaronTiesMock(){
+        // Mocking the players and their hands
+        Player user = Mockito.mock(Player.class);
+        Player opponent = Mockito.mock(Player.class);
+
+        Hand userHand = Mockito.mock(Hand.class);
+        Hand opponentHand = Mockito.mock(Hand.class);
+
+        // Setting up the hands
+        when(user.getHand()).thenReturn(userHand);
+        when(opponent.getHand()).thenReturn(opponentHand);
+
+        when(userHand.peek(0)).thenReturn(Card.HANDMAIDEN);
+        when(opponentHand.peek(0)).thenReturn(Card.HANDMAIDEN);
+        
+        // Action: User uses Baron on opponent
+        GameActions gameActions = new GameActions() {}; 
+        gameActions.useBaron(user, opponent);
+
+        // Assertion: Opponent should be eliminated
+        Assertions.assertTrue(user.getHand().hasCards()
+            && opponent.getHand().hasCards(), "Nothing happens");   
+    }
+
+    @Test
+    void testBaronProtectedMock(){
+        // Mocking the players and their hands
+        Player user = Mockito.mock(Player.class);
+        Player opponent = Mockito.mock(Player.class);
+
+        Hand userHand = Mockito.mock(Hand.class);
+        Hand opponentHand = Mockito.mock(Hand.class);
+
+        // Setting up the hands
+        when(user.getHand()).thenReturn(userHand);
+        when(opponent.getHand()).thenReturn(opponentHand);
+
+        when(userHand.peek(0)).thenReturn(Card.GUARD);
+        when(opponentHand.peek(0)).thenReturn(Card.GUARD);
+        
+        // Action: User uses Baron on opponent
+        opponent.switchProtection();
+        GameActions gameActions = new GameActions() {}; 
+        gameActions.useBaron(user, opponent);
+
+        // Assertion: Opponent should be eliminated
+        Assertions.assertTrue(user.getHand().hasCards()
+            && opponent.getHand().hasCards(), "Nothing happens"); 
     }
 }
