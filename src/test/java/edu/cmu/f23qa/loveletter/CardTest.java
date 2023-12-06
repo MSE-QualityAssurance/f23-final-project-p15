81af783811cf5a48a915d939881bbc0dbc757a21
tianqiZ0507 <148793678+tianqiZ0507@users.noreply.github.com>
2023-12-06 04:15:00 +0000

    Updated all 4 cases testing the Baron usage

--- src/test/java/edu/cmu/f23qa/loveletter/CardTest.java
+++ src/test/java/edu/cmu/f23qa/loveletter/CardTest.java
@@ -26,6 +26,57 @@ public class CardTest {
         opponent.getHand().add(Card.GUARD);
         GameActions gameActions = new GameActions() {}; 
         gameActions.useBaron(user, opponent);
-        Assertions.assertTrue(opponent.getHand().hasCards() == false, "Opponent should be eliminated");
+        Assertions.assertTrue(opponent.getHand().hasCards() == false, 
+            "Opponent should be eliminated");
+    }
+
+    /*
+     * After drawing, I discard the Baron and choose [normal player].
+     * My card is a Priest, and  [normal player] is a King. Since the 
+     * [normal player] card has a higher value, I am knocked out of the round.
+     */
+    @Test
+    void testBaronOpponentWins(){
+        Player user = new Player("User");
+        user.getHand().add(Card.PRIEST);
+        Player opponent = new Player("Opponent");
+        opponent.getHand().add(Card.KING);
+        GameActions gameActions = new GameActions() {}; 
+        gameActions.useBaron(user, opponent);
+        Assertions.assertTrue(user.getHand().hasCards() == false, "User should be eliminated");
+    }
+
+    /*
+     * I use the Baron and choose a player who also has a Baron. 
+     * Since we both have cards of equal value, nothing happens, 
+     * and no one is knocked out.
+     */
+    @Test
+    void testBaronTies(){
+        Player user = new Player("User");
+        user.getHand().add(Card.HANDMAIDEN);
+        Player opponent = new Player("Opponent");
+        opponent.getHand().add(Card.HANDMAIDEN);
+        GameActions gameActions = new GameActions() {}; 
+        gameActions.useBaron(user, opponent);
+        Assertions.assertTrue(user.getHand().hasCards()
+            && opponent.getHand().hasCards(), "Nothing happens");    
+    }
+
+    /*
+     * I cannot use the Baron on a [abnormal player] 
+     * who is currently protected by a Handmaid.
+     */
+    @Test
+    void testBaronProtected(){
+        Player user = new Player("User");
+        user.getHand().add(Card.KING);
+        Player opponent = new Player("Opponent");
+        opponent.getHand().add(Card.KING);
+        opponent.switchProtection();
+        GameActions gameActions = new GameActions() {}; 
+        gameActions.useBaron(user, opponent);
+        Assertions.assertTrue(user.getHand().hasCards() == true 
+            && opponent.getHand().hasCards() == true, "Nothing happens");    
     }
 }
