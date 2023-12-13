30a7374a1be45dfd759cbd1c99929e5891562521
ValentinaTang <wantingt@andrew.cmu.edu>
2023-12-13 21:26:11 +0000

    rename test files

--- src/test/java/edu/cmu/f23qa/loveletter/GameTest.java
+++ src/test/java/edu/cmu/f23qa/loveletter/GameTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class CheckGameEndTest {
+public class GameTest {
 
     private ByteArrayOutputStream outputStream;
 
